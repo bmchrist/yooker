5a571006c80924c9bc01636412609df6203ba814
Ben Christensen <ben@joinhandshake.com>
2020-03-21 23:18:13 -0700

    Deal button does something now

--- lib/yooker_web/live/game_live.ex
+++ lib/yooker_web/live/game_live.ex
@@ -1,22 +1,21 @@
 defmodule YookerWeb.GameLive do
   use Phoenix.LiveView
+  alias Yooker.State
 
   require Logger
 
   def render(assigns) do
-    ~L"""
-    <h1>LiveView is awesome!</h1>
-    """
-    #YookerWeb.GameView.render("index.html", assigns)
+    YookerWeb.GameView.render("index.html", assigns)
   end
 
   def mount(_session, params, socket) do
-    {:ok, assign(socket, state: %Yooker.State{})}
+    # TODO(bmchrist) figure out why we need to manually define the default layout)
+    {:ok, assign(socket, layout: {YookerWeb.LayoutView, "app.html"}, state: %Yooker.State{})}
   end
 
-  #def handle_event("deal", _event, %{assigns: assigns} = socket) do # todo - why do we do the assigns = socket thing
-    #Logger.info("deal")
-    #new_state = State.deal(assigns.state)
-    #{:noreply, assign(socket, state: new_state)}
-  #end
+  def handle_event("deal", _event, %{assigns: assigns} = socket) do # todo - why do we do the assigns = socket thing
+    Logger.info("deal")
+    new_state = State.deal(assigns.state)
+    {:noreply, assign(socket, state: new_state)}
+  end
 end
