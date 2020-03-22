c18224fa6a36bbc9fa774234201c2517627508d6
Ben Christensen <ben@joinhandshake.com>
2020-03-21 19:50:26 -0700

    Install LiveView

--- lib/yooker_web.ex
+++ lib/yooker_web.ex
@@ -21,6 +21,7 @@ defmodule YookerWeb do
     quote do
       use Phoenix.Controller, namespace: YookerWeb
 
+      import Phoenix.LiveView.Controller
       import Plug.Conn
       import YookerWeb.Gettext
       alias YookerWeb.Router.Helpers, as: Routes
@@ -35,6 +36,7 @@ defmodule YookerWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+      import Phoenix.LiveView.Helpers
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
@@ -50,6 +52,7 @@ defmodule YookerWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
